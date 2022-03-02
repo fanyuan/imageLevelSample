c57157c6ae3d165e5b0d7f75574fb434438a8cfa
fly <lanseyoukong638@163.com>
2022-03-02 15:37:26 +0800

    code is inited

---
+++ app/src/main/java/com/imagelevel/sample/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.imagelevel.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var imageView:ImageView;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.iv_display)
+    }
+
+    var level = 0;
+    fun click(view: View) {
+        if(level >= 8){
+            level = 0;
+        }
+        imageView.setImageLevel(level);
+        level++
+    }
+}
